02b1a1f0ab473ae01e3238921df82dde82e5aaa5
abdulhakim10 <s.abdulhakim10@gmail.com>
2022-08-20 02:43:14 +0600

    updatePhoneQuantity function added in phone.js file

--- js/phone.js
+++ js/phone.js
@@ -1,11 +1,26 @@
+function updatePhoneQuantity(isIncrese) {
+    const phoneQuantityField = document.getElementById('ph-quantity-field');
+    const phoneQuantityString = phoneQuantityField.value;
+    const previousPhoneQuantity = parseInt(phoneQuantityString);
 
+    let newPhoneQuantity;
+
+    if (isIncrese === true) {
+        newPhoneQuantity = previousPhoneQuantity + 1;
+    }
+    else {
+        newPhoneQuantity = previousPhoneQuantity - 1;
+    }
+
+    phoneQuantityField.value = newPhoneQuantity;
+}
 
 
 
 document.getElementById('phone-plus-btn').addEventListener('click', function () {
-
+    updatePhoneQuantity(true);
 })
 
 document.getElementById('phone-minus-btn').addEventListener('click', function () {
-
+    updatePhoneQuantity(false);
 })

81e858971a98f6ba40d9f85500eb92592d6c01b9
abdulhakim10 <s.abdulhakim10@gmail.com>
2022-08-20 01:59:28 +0600

    index.html, style.css and case.js files are added

---
+++ js/case.js
@@ -0,0 +1,39 @@
+function updateCaseNumber(isIncrese) {
+    const caseNumberField = document.getElementById('case-value-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    let newCaseNumber;
+
+    if (isIncrese === true) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
+
+    caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+
+}
+
+
+function updateCaseTotalPrice(newCaseNumber) {
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+}
+
+document.getElementById('case-plus-btn').addEventListener('click', function () {
+
+    const newCaseNumber = updateCaseNumber(true);
+
+    updateCaseTotalPrice(newCaseNumber);
+})
+
+document.getElementById('case-minus-btn').addEventListener('click', function () {
+
+    const newCaseNumber = updateCaseNumber(false);
+
+    updateCaseTotalPrice(newCaseNumber);
+})
